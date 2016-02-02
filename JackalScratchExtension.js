a714adc276dc6bc0fbaa7cfa499fca82ca5b6d4a
jackalkao <shinjikao@gmail.com>
2016-02-02 11:10:27 +0800

    scratch modify2

--- JackalScratchExtension.js
+++ JackalScratchExtension.js
@@ -15,16 +15,16 @@ new (function() {
     };
 
     ext.getRobotYoYo = function(Username, callback) {
-        console.log("TEST")
+        console.log("TEST");
         // Make an AJAX call to the Open Weather Maps API
         $.ajax({
               //url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              url: 'http://192.168.43.97:8080/?getYoyo='+ Username,
-              dataType: 'jsonp',
+              url: 'http://192.168.43.97:8080/?getYoyo='+ Username, dataType: 'jsonp',
               success: function( responsedata ) {
-                  console.log('success')
+                  console.log('success');
+                  $("#div1").html('success');
                   // Got the data - parse it and return the temperature
-                  console.log(responsedata)
+                  console.log(responsedata);
               }
         });
     };
